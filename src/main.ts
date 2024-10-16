4fe866e3021b200661fcc6b22af2e9dfc881872a
Ian-Stentz <ianstentz25@gmail.com>
2024-10-16 14:16:32 -0700

    Step 2 complete
    Energy button adds energy to a display that say "{x} energy"

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,22 @@ app.append(header);
 
 const buttonEmoji = "⚡";
 
+let counter: number = 0;
+
+interface Event { handleEvent() : void };
+
+const buttonClick : Event = {
+    handleEvent() {
+        counter += 1;
+        counterDisplay.innerHTML = `${counter} energy`
+    }
+}
+
 const button = document.createElement("button");
 button.innerHTML = buttonEmoji;
+button.addEventListener("click", buttonClick);
 app.append(button);
+
+const counterDisplay = document.createElement("div");
+counterDisplay.innerHTML = `${counter} energy`
+app.append(counterDisplay);
