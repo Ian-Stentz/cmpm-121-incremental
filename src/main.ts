40f2bc14833c391c42d1341d79231cfb3d5fb1e0
Ian-Stentz <ianstentz25@gmail.com>
2024-10-16 13:58:54 -0700

    Step 1 complete
    Added a lightning bolt button!

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,9 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const buttonEmoji = "⚡";
+
+const button = document.createElement("button");
+button.innerHTML = buttonEmoji;
+app.append(button);
